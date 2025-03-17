9a1e8f4ca69005e5afbe3f7ed6843bf9c6de85af
zs <13554963+zs_12_0@user.noreply.gitee.com>
2025-03-17 16:36:44 +0800

    查询用户无脱敏信息

--- admin/src/main/java/org/zsbscs/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/zsbscs/shortlink/admin/controller/UserController.java
@@ -1,13 +1,13 @@
 package org.zsbscs.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.zsbscs.shortlink.admin.common.convention.result.Result;
 import org.zsbscs.shortlink.admin.common.convention.result.Results;
-import org.zsbscs.shortlink.admin.common.enums.UserErrorCodeEnum;
+import org.zsbscs.shortlink.admin.dto.resp.UserActualRespDTO;
 import org.zsbscs.shortlink.admin.dto.resp.UserRespDTO;
 import org.zsbscs.shortlink.admin.service.UserServie;
 
@@ -16,19 +16,21 @@ import org.zsbscs.shortlink.admin.service.UserServie;
 public class UserController {
 
     private final UserServie userService;
+
     /**
-     *
      * 根据用户名查找用户
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public Result<UserRespDTO> getUserByUername(@PathVariable("username") String username) {
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         UserRespDTO result = userService.getUserByUsername(username);
-        if(result == null) {
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        }
-        else {
-            return Results.success(result);
-        }
-
+        return Results.success(result);
+    }
+    /**
+     * 根据用户名查找用户真实信息
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        return  Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
+
